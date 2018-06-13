714d31230325c59ed47336007a50a33351fbd942
Wilson <bianliuzhu@gmail.com>
2018-06-13 16:29:45 +0800

    Wire up all the things

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="hello">
+    <div class="container">
+      <div class="row">
+        <input class="col-sm-8" type="text" id="wadoURL" placeholder="Enter WADO URL" value="http://127.0.0.1/testDICOM/CTStudy/1.5191KB.DCM">
+        <button class="col-sm-3 btn" type="button" id="downloadAndView">Download and View</button>
+      </div>
+      <div id="loadProgress">Image Load Progress:</div>
+      <div style="width:512px; height:512px; position:relative; color:white; display:inline-block; border-style:solid; border-color:black;"
+        oncontextmenu="return false" class='disable-selection noIbar' unselectable='on' onselectstart='return false;' onmousedown='return false;'>
+        <div id="dicomImage" style="width:512px;height:512px;top:0px;left:0px; position:absolute">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as cornerstone from "cornerstone-core";
+import * as dicomParser from "dicom-parser";
+import * as cornerstoneWADOImageLoader from "../../static/dist/cornerstoneWADOImageLoader.js";
+cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
+var config = {
+  webWorkerPath: "/static/dist/cornerstoneWADOImageLoaderWebWorker.js",
+  taskConfiguration: {
+    decodeTask: {
+      codecsPath: "/static/dist/cornerstoneWADOImageLoaderCodecs.js"
+    }
+  }
+};
+cornerstoneWADOImageLoader.webWorkerManager.initialize(config);
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    loadAndViewImage(imageId) {
+      var element = document.getElementById("dicomImage");
+      cornerstone.loadAndCacheImage(imageId).then(
+        function(image) {
+          console.log(image);
+          var viewport = cornerstone.getDefaultViewportForImage(element, image);
+          cornerstone.displayImage(element, image, viewport);
+        },
+        function(err) {
+          alert(err);
+        }
+      );
+    }
+  },
+  mounted() {
+    const _this = this;
+    var element = document.getElementById("dicomImage");
+    cornerstone.enable(element);
+    // 监听 downloadAndView 按钮 拼接url 调用 loadAndViewImage 函数
+    let url = document.getElementById("wadoURL").value;
+    // 拼接url
+    url = "wadouri:" + url;
+    // 调用这个函数加载像,和激活工具
+    _this.loadAndViewImage(url);
+
+    // Dicom 加载 进度
+    cornerstone.events.addEventListener(
+      "cornerstoneimageloadprogress",
+      function(event) {
+        const eventData = event.detail;
+        const loadProgress = document.getElementById("loadProgress");
+        loadProgress.textContent = `Image Load Progress: ${
+          eventData.percentComplete
+        }%`;
+      }
+    );
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
